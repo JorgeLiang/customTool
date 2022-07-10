4edd1cf69675b74bb0ce9e578fe20fef0c0cca09
JorgeLiang <276224495@qq.com>
2022-07-10 14:49:07 +0800

    fix SortPictureList compare -3

--- photoAlbum/src/main/java/com/jorge/photoalbum/SortPictureList.java
+++ photoAlbum/src/main/java/com/jorge/photoalbum/SortPictureList.java
@@ -14,11 +14,23 @@ public class SortPictureList implements Comparator {
     public int compare(Object o1, Object o2) {
 //        String path1 = o1.toString();
 //        String path2 = o2.toString();
-        File file1 = new File(o1.toString());
-        File file2 = new File(o2.toString());
-        return (int) (file2.lastModified() - file1.lastModified());
+//        File file1 = new File(o1.toString());
+//        File file2 = new File(o2.toString());
+
 //        if (file1.lastModified() < file2.lastModified()) {
 //            return 1;
 //        } else return -1;
+
+//        return (int) (file2.lastModified() - file1.lastModified());
+
+        File file1 = new File(o1.toString());
+        File file2 = new File(o2.toString());
+        if (file1.lastModified() < file2.lastModified()) {
+            return 1;
+        } else if (file1.lastModified() > file2.lastModified()){
+            return -1;
+        }else {
+            return 0;
+        }
     }
 }
